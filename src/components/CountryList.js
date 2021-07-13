18f4adca63748886ce3c76df289d745a452defab
unknown <siminatabac@yahoo.com>
2021-07-13 11:53:11 +0300

    prepared the url for cities

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function CountryList() {
-  const [countriesList, setCountriesList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const URL =
-    "https://restcountries.eu/rest/v2/region/europe";
+    const [countriesList, setCountriesList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const URL =
+        "https://restcountries.eu/rest/v2/region/europe";
 
-  useEffect(() => {
-    axios.get(URL).then((response) => {
-      console.log(response.data);
-      setCountriesList(response.data);
-      setIsLoading(false);
-    });
-  }, []);
+    useEffect(() => {
+        axios.get(URL).then((response) => {
+            console.log(response.data);
+            setCountriesList(response.data);
+            setIsLoading(false);
+        });
+    }, []);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
 
-  return <section>{countriesList.map((country) => {
-      return (
-        <ul key={country.name}>
-          <Link to={`/cities/${country.alpha2Code}`}>
-            <li>{country.name}</li>
-          </Link>
-        </ul>
-      );
-  })}</section>;
+    return <section>
+        <ul>{countriesList.map((country) => {
+            const urlToCities = "/cities/" + country.alpha2Code
+            return (
+                <Link to={{ pathname: urlToCities, state: country.alpha2Code }}>
+                    <li key={country.name}>{country.name}</li>
+                </Link>
+            );
+        })}</ul></section>;
 }
 
 export default CountryList;
