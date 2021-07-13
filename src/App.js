28c6c9162e5a58240445c8df4f9628960dbedfa5
Silviu Bocsa <silviubocsa@gmail.com>
2021-07-13 15:37:29 +0100

    Cards&Country Flags

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import Navbar from './components/Navbar';
 import { Route, Switch } from 'react-router-dom'
 import CitiesList from "./components/CitiesList"
 import CountryList from './components/CountryList';
+import Layout from './components/Layout';
 
 
 function App() {
   return (
-
-    <div >
-      <Navbar />
+      <Layout>
       <Switch>
         <Route path="/countries" exact><CountryList/></Route>
         <Route exact path="/cities/:countryCode" component={CitiesList} />
         <Route path="/favorites" exact></Route>
       </Switch>
-    </div>
+      </Layout>
 
   );
 }
