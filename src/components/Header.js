b1945f062e0e2ac6523821ea767d00d1bd34d836
FloareDeMai <matei.g.florentina@gmail.com>
2021-07-14 13:16:53 +0300

    link to introduction page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,23 +6,28 @@ import classes from './Header.module.css';
 
 function Header() {
     return (
-        <header className={classes.header}>
+      <header className={classes.header}>
         <div className={classes.headerpicture}></div>
-        <div className={classes.text}><h1>Journeyfy</h1></div>
-        <div className={classes.textmotto}><h5>Trip your journey</h5></div>
-            <nav>
-            <ul>
-                <li className={classes.link}>
-                    <Link to="/countries">Country List</Link>
-                </li>
+        <div className={classes.text}>
+          <h1>
+            <Link to="/">Journeyfy</Link>
+          </h1>
+        </div>
+        <div className={classes.textmotto}>
+          <h5>Trip your journey</h5>
+        </div>
+        <nav>
+          <ul>
+            <li className={classes.link}>
+              <Link to="/countries">Country List</Link>
+            </li>
 
-                <li className={classes.link}>
-                    <Link to="/favorites">Favorites Activities</Link>
-                </li>
-
-            </ul>
-            </nav>
-        </header>
+            <li className={classes.link}>
+              <Link to="/favorites">Favorites Activities</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
     
 }
