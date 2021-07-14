c47cd9a5d3f7f3a132d05ac0b858fa5e28e0e1a1
Silviu Bocsa <silviubocsa@gmail.com>
2021-07-14 14:53:29 +0100

    Changes to introductionPage

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,22 @@ function Header() {
         <header className={classes.header}>
         <div className={classes.headerpicture}></div>
         <div className={classes.text}><h1>Journeyfy</h1></div>
-        <div className={classes.textmotto}><h5>Trip your journey</h5></div>
+        <div className={classes.textmotto}><h5>Plan your escape</h5></div>
             <nav>
             <ul>
                 <li className={classes.link}>
-                    <Link to="/countries">Country List</Link>
+                    <Link to="/countries">Countries</Link>
                 </li>
 
                 <li className={classes.link}>
-                    <Link to="/favorites">Favorites Activities</Link>
+                    <Link to="/favorites">Activities</Link>
                 </li>
+
+                <li className={classes.link}>
+                    <Link to="/journeys">Journeys</Link>
+                </li>
+
+                <li className={classes.link}></li>
 
             </ul>
             </nav>
