cb45a14f09e9ad148623627bb4be2891af825b80
unknown <siminatabac@yahoo.com>
2021-07-14 10:33:11 +0300

    filtered cities

--- src/components/City.js
+++ src/components/City.js
@@ -7,23 +7,22 @@ import CityNames from './CityNames'
 function City(props) {
     const coordinates = props.cities.map((city) => city.coordinates)
     coordinates.map((city, i) => console.log(city.latitude, city.longitude))
-    const cityNames = props.cities.map((city) => <li>{city.name}</li>)
+    const cityNames = props.cities.map((city) => city.name)
+    const filteredCities = cityNames.filter((city) => !city.includes("Sector"))
 
     let urlToActivities = "";
     let latitude = 0;
     let longitude = 0;
     coordinates.map((city, i) => {
-        urlToActivities = "/activities/" + city.latitude + "/" + city.longitude 
+        urlToActivities = "/activities/" + city.latitude + "/" + city.longitude
         latitude = city.latitude
-        longitude = city.longitude 
+        longitude = city.longitude
     })
     return (
         <div>
-            <ul>
-            <Link to={{pathname: urlToActivities, state:{latitude: latitude, longitude: longitude}}}>
-                {cityNames}
+            <Link to={{ pathname: urlToActivities, state: { latitude: latitude, longitude: longitude } }}>
+                <li>{filteredCities}</li>
             </Link>
-            </ul>
         </div>
     )
 }
