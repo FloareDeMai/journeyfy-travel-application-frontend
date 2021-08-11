166a720ffcd0a4f1c97fd0eefdd5e2ebbc812f83
unknown <siminatabac@yahoo.com>
2021-08-11 15:37:28 +0300

    fetch hotels

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import CitiesList from "./components/CitiesList"
 import CountryList from './components/CountryList';
 import Layout from './components/Layout';
@@ -6,21 +6,23 @@ import ActivityList from './components/ActivityList';
 import IntroductionPage from './components/IntroductionPage';
 import About from './components/About';
 import Favorites from './components/Favorites';
+import Hotels from "./components/Hotels";
 
 function App() {
-  return (
-      <Layout>
-      <Switch>
-        <Route path="/" exact><IntroductionPage></IntroductionPage></Route>
-        <Route path="/countries" exact><CountryList/></Route>
-        <Route exact path="/cities/:countryCode" component={CitiesList} />
-        <Route path="/favorites" exact><Favorites></Favorites></Route>
-        <Route path="/about" exact><About></About></Route>
-        <Route exact path="/activities/:latitude/:longitude" component={ActivityList}></Route>
+    return (
+        <Layout>
+            <Switch>
+                <Route path="/" exact><IntroductionPage></IntroductionPage></Route>
+                <Route path="/countries" exact><CountryList/></Route>
+                <Route exact path="/cities/:countryCode" component={CitiesList}/>
+                <Route path="/favorites" exact><Favorites></Favorites></Route>
+                <Route path="/about" exact><About></About></Route>
+                <Route exact path="/activities/:latitude/:longitude" component={ActivityList}/>
+                <Route exact path="/hotels/:cityName" component={Hotels}/>
 
-      </Switch>
-      </Layout>
-  );
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Card from "../ui/Card";
+import classes from './CountryList.module.css';
 
-function Hotels() {
+function Hotels(props) {
+    const [hotels, setHotels] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    let cityNameParam = props.match.params.cityName
     useEffect(() => {
-        axios.get(URL).then((response) => {
-            setCountriesList(response.data);
-            setIsLoading(false);
-        });
-    }, []);
+        fetch(`http://localhost:8080/hotels/${cityNameParam}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setHotels(data)
+                setIsLoading(false)
+            })
+    }, [cityNameParam]);
+    if (isLoading) {
+        return "Loading..."
+    }
     return (
         <div>
-            
+            <ul className={classes.list}>
+                {hotels.map(hotel =>
+
+                    <Card>
+                        <li className={classes.cityName}><h3>{hotel.name}</h3></li>
+                    </Card>
+
+                )}
+            </ul>
+
         </div>
     )
 }
