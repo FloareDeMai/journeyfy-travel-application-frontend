4125d5f762b9cd11efd97abea475ccb9af8277de
Silviu Bocsa <silviubocsa@gmail.com>
2021-08-23 18:13:05 +0300

    REMASTER project with design framework

--- src/components/CountryImage.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-
-function CountryImage(props) {
-    const URL = `https://restcountries.eu/data/${props.image}.svg`;
-    return <img src={URL} alt={props.name}></img>;
-}
-export default CountryImage;
